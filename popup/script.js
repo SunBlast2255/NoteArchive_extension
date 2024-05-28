6b39e5dd55f8f30561b1ce3e986a30b4bc2368c3
SunBlast2215 <sunflash3412@gmail.com>
2024-05-28 14:43:27 +0300

    Editor added

--- popup/script.js
+++ popup/script.js
@@ -1,5 +1,23 @@
 function displayNote(){
-    
+    chrome.storage.local.get(["Count"]).then((result) => {
+        if(result.Count == 0 || result.Count == undefined || result.Count == null){
+            let span = document.createElement("span");
+            span.innerHTML = "You have no notes";
+            span.style.fontSize = "16px";
+            document.getElementById("note-container").appendChild(span);
+            chrome.storage.local.set({"Count": "0" }, function(){});
+        }else if(result.Count > 0 && result.Count != undefined && result.Count != null){
+            //load notes
+        }
+    });
+}
+
+function openEditor(){
+    document.getElementById("editor-window").style.display = "flex";
+}
+
+function closeEditor(){
+    document.getElementById("editor-window").style.display = "none";
 }
 
 function addNote(){
@@ -13,3 +31,11 @@ function deleteNote(){
 window.onload = function() {
     displayNote();
 }
+
+document.getElementById("add-btn").addEventListener("click", function(){
+    openEditor();
+});
+
+document.getElementById("exit-btn").addEventListener("click", function(){
+    closeEditor();
+});
