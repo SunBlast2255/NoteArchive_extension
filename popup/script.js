85e559f8dc302b6fd21248f8e0085f99b07eac92
SunBlast2215 <sunflash3412@gmail.com>
2024-05-29 00:35:36 +0300

    Added a feature to display notes on the home screen and a feature to save notes to chrome local storage.

--- popup/script.js
+++ popup/script.js
@@ -1,14 +1,46 @@
 function displayNote(){
     chrome.storage.local.get(["Count"]).then((result) => {
         if(result.Count == 0 || result.Count == undefined || result.Count == null){
+            document.getElementById("note-container").innerHTML = "";
+            document.getElementById("notes").innerHTML = "0";
+
             let span = document.createElement("span");
             span.innerHTML = "You have no notes";
-            span.style.fontSize = "16px";
+            span.style = "margin: 5px; font-size: 16px;"
             document.getElementById("note-container").appendChild(span);
+
             chrome.storage.local.set({"Count": "0" }, function(){});
         }else if(result.Count > 0 && result.Count != undefined && result.Count != null){
-            //load notes
+            document.getElementById("note-container").innerHTML = "";
+
+            let count;
+            chrome.storage.local.get("Count", function(result) {
+                count = result.Count;
+                document.getElementById("notes").innerHTML = count;
+
+                getAllNotes(function(notes) {
+                        for (let noteId in notes) {
+                            if (notes.hasOwnProperty(noteId) && noteId !== "Count") {
+                                console.log('Note ID:', noteId, 'Text:', notes[noteId]);
+
+                                let div = document.createElement("div");
+                                div.style = "display: flex; width: 100px; height: 100px; border: 1px solid black; border-radius: 5px; text-overflow: ellipsis; overflow: hidden; cursor: pointer; margin-top: 5px;";
+                                let span = document.createElement("span");
+                                span.innerHTML = notes[noteId];
+                                div.appendChild(span);
+                                document.getElementById("note-container").appendChild(div);
+                            }
+                        }
+                });
+                
+            });
         }
+    });
+}
+
+function getAllNotes(callback){
+    chrome.storage.local.get(null, function(result) {
+        callback(result);
     });
 }
 
@@ -21,7 +53,22 @@ function closeEditor(){
 }
 
 function addNote(){
+    let date = new Date();
 
+    let text = document.getElementById("textarea").value;
+    let noteId = `${date.getDate()}${date.getMonth() + 1}${date.getFullYear()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+
+    let note = {};
+    note[noteId] = text;
+
+    chrome.storage.local.set(note, function(){
+        chrome.storage.local.get("Count", function(result) {
+            let count = parseInt(result.Count) + 1;
+            chrome.storage.local.set({"Count": count}, function(){
+                displayNote();
+            });
+        });
+    });
 }
 
 function deleteNote(){
