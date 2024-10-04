a127b73d2815c49b9a0c677a0f5b4457b31d5ee1
SunBlast2215 <sunflash3412@gmail.com>
2024-10-05 00:20:32 +0300

    Revert "Bugs fixed"
    This reverts commit e150b7159e412e4af0a445f75f71effe11ac842a.

--- popup/script.js
+++ popup/script.js
@@ -355,7 +355,7 @@ document.getElementById("copy-selected-viewer-text").addEventListener("click", f
 //Context
 
 window.oncontextmenu = function(){
-    //return false;
+    return false;
 }
 
 function openContextMenu(textareaID, contextID, e){
